45572d70d28a34545e77e8facdc650567d911e56
Crowdbotics <team@crowdbotics.com>
2023-11-06 06:58:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-band-44361",
+  url: "https://odd-band-44361.botics.co",
+  api: "https://odd-band-44361.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
